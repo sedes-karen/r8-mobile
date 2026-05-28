df4aecb7734feb3a027d0a0328ce316baaf0e2ba
Bowuigi <bowuigi@vern.cc>
2026-05-28 20:35:19 -0300

    feat: Organized navigation

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,16 +1,105 @@
-import { createStaticNavigation } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { LoginScreen } from "../screens/auth/LoginScreen";
-import { SignUpScreen } from "../screens/auth/SignUpScreen";
-import { PasswordResetScreen } from "../screens/auth/PasswordResetScreen";
-import { AuthInfoProvider, useIsAuthenticated, useIsNotAuthenticated } from "../features/auth/info";
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const LoginStack = createNativeStackNavigator({
+// Los providers
+import { AuthInfoProvider, useIsArtist, useIsAuthenticated, useIsLabel, useIsNotAuthenticated } from '../features/auth/info';
+// Fin de los providers
+
+// Las pantallas
+import { AuthLoginScreen } from '../screens/Auth/Login';
+import { AuthSignUpScreen } from '../screens/Auth/SignUp';
+import { AuthPasswordResetScreen } from '../screens/Auth/PasswordReset';
+import { ArtistPromosPlayerScreen } from '../screens/Artist/Promos/Player';
+import { ArtistPromosDetailsScreen } from '../screens/Artist/Promos/Details';
+import { ArtistPromosFeedbackScreen } from '../screens/Artist/Promos/Feedback';
+import { ArtistPromosLikedTracksScreen } from '../screens/Artist/Promos/LikedTracks';
+import { ArtistProfileViewScreen } from '../screens/Artist/Profile/View';
+import { ArtistProfileEditScreen } from '../screens/Artist/Profile/Edit';
+import { LabelDashboardScreen } from '../screens/Label/Dashboard';
+import { LabelProfileViewScreen } from '../screens/Label/Profile/View';
+import { LabelProfileEditScreen } from '../screens/Label/Profile/Edit';
+import { LabelAnalyticsScreen } from '../screens/Label/Analytics';
+import { LabelReleasesListScreen } from '../screens/Label/Releases/List';
+import { LabelReleasesNewScreen } from '../screens/Label/Releases/New';
+import { LabelReleasesDetailsScreen } from '../screens/Label/Releases/Details';
+import { LabelReleasesEditScreen } from '../screens/Label/Releases/Edit';
+import { LabelReleasesPromosListScreen } from '../screens/Label/Releases/Promos/List';
+import { LabelReleasesPromosNewScreen } from '../screens/Label/Releases/Promos/New';
+import { LabelReleasesPromosDetailsScreen } from '../screens/Label/Releases/Promos/Details';
+import { LabelReleasesPromosEditScreen } from '../screens/Label/Releases/Promos/Edit';
+import { LabelRecipientListsListScreen } from '../screens/Label/RecipientLists/List';
+import { LabelRecipientListsNewScreen } from '../screens/Label/RecipientLists/New';
+import { LabelRecipientListsDetailsScreen } from '../screens/Label/RecipientLists/Details';
+import { LabelRecipientListsEditScreen } from '../screens/Label/RecipientLists/Edit';
+import { LabelRecipientListsFeedbackScreen } from '../screens/Label/RecipientLists/Feedback';
+import { LabelRecipientListsBulkUploadScreen } from '../screens/Label/RecipientLists/BulkUpload';
+// Fin de las pantallas
+
+const AuthStack = createNativeStackNavigator({
   initialRouteName: 'Login',
   screens: {
-    Login: LoginScreen,
-    SignUp: SignUpScreen,
-    PasswordReset: PasswordResetScreen,
+    Login: AuthLoginScreen,
+    SignUp: AuthSignUpScreen,
+    PasswordReset: AuthPasswordResetScreen,
+  }
+} as const);
+
+const ArtistPromosStack = createNativeStackNavigator({
+  initialRouteName: 'Player',
+  screens: {
+    Player: ArtistPromosPlayerScreen,
+    Detail: ArtistPromosDetailsScreen,
+    Feedback: ArtistPromosFeedbackScreen,
+    LikedTracks: ArtistPromosLikedTracksScreen,
+  },
+});
+
+const ArtistProfileStack = createNativeStackNavigator({
+  initialRouteName: 'View',
+  screens: {
+    View: ArtistProfileViewScreen,
+    Edit: ArtistProfileEditScreen,
+  },
+});
+
+const LabelProfileStack = createNativeStackNavigator({
+  initialRouteName: 'View',
+  screens: {
+    View: LabelProfileViewScreen,
+    Edit: LabelProfileEditScreen,
+  },
+});
+
+const LabelReleasesPromosStack = createNativeStackNavigator({
+  initialRouteName: 'List',
+  screens: {
+    List: LabelReleasesPromosListScreen,
+    New: LabelReleasesPromosNewScreen,
+    Details: LabelReleasesPromosDetailsScreen,
+    Edit: LabelReleasesPromosEditScreen,
+  },
+});
+
+const LabelReleasesStack = createNativeStackNavigator({
+  initialRouteName: 'List',
+  screens: {
+    List: LabelReleasesListScreen,
+    New: LabelReleasesNewScreen,
+    Details: LabelReleasesDetailsScreen,
+    Edit: LabelReleasesEditScreen,
+    Promos: LabelReleasesPromosStack,
+  }
+});
+
+const LabelRecipientListsStack = createNativeStackNavigator({
+  initialRouteName: 'List',
+  screens: {
+    List: LabelRecipientListsListScreen,
+    New: LabelRecipientListsNewScreen,
+    Details: LabelRecipientListsDetailsScreen,
+    Edit: LabelRecipientListsEditScreen,
+    Feedback: LabelRecipientListsFeedbackScreen,
+    BulkUpload: LabelRecipientListsBulkUploadScreen,
   }
 } as const);
 
@@ -18,18 +107,35 @@ const RootStack = createNativeStackNavigator({
   initialRouteName: 'Dashboard',
   screens: {
     Auth: {
-      screen: LoginStack,
+      screen: AuthStack,
       if: useIsNotAuthenticated,
     },
-    Dashboard: {
-      screen: undefined as any, // TODO, may crash
-      if: useIsAuthenticated,
-    }
+  },
+  groups: {
+    Artist: {
+      if: useIsArtist,
+      screens: {
+        Promos: ArtistPromosStack,
+        Profile: ArtistProfileStack,
+      },
+    },
+    Label: {
+      if: useIsLabel,
+      screens: {
+        Profile: LabelProfileStack,
+        Dashboard: LabelDashboardScreen,
+        Analytics: LabelAnalyticsScreen,
+        Releases: LabelReleasesStack,
+        RecipientLists: LabelRecipientListsStack,
+      },
+    },
   }
-} as const).with(({ Navigator }) => (
-  <AuthInfoProvider>
-    <Navigator />
-  </AuthInfoProvider>
-));
+} as const).
+  // Todos los providers van acá abajo
+  with(({ Navigator }) => (
+    <AuthInfoProvider>
+      <Navigator />
+    </AuthInfoProvider>
+  ));
 
 export const Navigation = createStaticNavigation(RootStack);
