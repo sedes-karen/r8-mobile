05b695ba5778d834ac5d0ccbc374a7183ed24f52
Ignacio_Galarraga <ignacio.g4l4rr4g4@gmail.com>
2026-06-05 00:32:21 -0300

    feat: atomo del avatar

--- src/components/atoms/Avatar/Avatar.style.ts
+++ src/components/atoms/Avatar/Avatar.style.ts
@@ -1,2 +1,29 @@
 import { StyleSheet } from 'react-native';
-import { colors, spacing, borderRadius } from '../../../constants/design';
+import { colors } from '../../../constants/design';
+
+export const styles = StyleSheet.create({
+    avatarContainer: {
+        overflow: 'hidden',
+        backgroundColor: colors.background,
+    },
+    avatarImage: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+    },
+    sm: {
+        width: 32,
+        height: 32,
+        borderRadius: 16,
+    },
+    md: {
+        width: 56,
+        height: 56,
+        borderRadius: 28,
+    },
+    lg: {
+        width: 96,
+        height: 96,
+        borderRadius: 48,
+    },
+});
--- src/components/atoms/Avatar/Avatar.tsx
+++ src/components/atoms/Avatar/Avatar.tsx
@@ -1,2 +1,33 @@
-import { View, Image, Text, Pressable } from 'react-native';
 import { styles } from './Avatar.style';
+import React from 'react';
+
+import { View, Image } from 'react-native';
+
+export type AvatarVariant = 'sm'|'md'|'lg';
+interface AvatarProps {
+    src?: string;
+    alt?: string;
+    variant?: AvatarVariant;
+}
+
+export default function Avatar({ src, variant = 'md' }: AvatarProps) {
+    const sizeClass = styles[variant];
+
+    const avatarSizes: Record<AvatarVariant, number> = {
+        sm: 32,
+        md: 56,
+        lg: 96
+    };
+    const pixelSize = avatarSizes[variant];
+
+    const imageSrc = src || `https://img.magnific.com/vector-premium/ilustracion-vectorial-capibara_792626-376.jpg?semt=ais_hybrid&w=740&q=80=${pixelSize}`;
+
+    return (
+        <View style={[styles.avatarContainer, sizeClass]}>
+            <Image
+                source={{ uri: imageSrc }}
+                style={styles.avatarImage}
+            />
+        </View>
+    );
+}
